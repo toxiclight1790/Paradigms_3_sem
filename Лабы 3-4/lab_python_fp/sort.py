e27e3b450e92a6442b92b88e5d5df5615882acb1
toxiclight1790 <62153725+toxiclight1790@users.noreply.github.com>
2023-12-17 23:55:44 +0300

    лабы 3-4

---
+++ Лабы 3-4/lab_python_fp/sort.py
@@ -0,0 +1,15 @@
+""" Task #4 """
+
+
+def main4():
+    print('\n---#4---')
+    data = [4, -30, 100, -100, 123, 1, 0, -1, -4]
+    result = sorted(data, key=abs, reverse=True)
+    print(result)
+
+    result_with_lambda = sorted(data, key=lambda a: abs(a), reverse=True)
+    print(result_with_lambda)
+
+
+if __name__ == '__main__':
+    main4()
